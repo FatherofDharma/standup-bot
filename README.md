44bbddb94cbacafd13135acfaf54f40ceee9bea7
Brian Hensley <brnhensley@gmail.com>
2021-04-01 08:47:28 -0700

    minor updates and readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# _Standup Bot_
+
+#### _A simple discord bot that posts alerts at specific times._
+
+#### _By Brian Hensley_
+#### _3-23-21_
+
+## _Description_
+
+This bot will post a message into a specefic discord channel, and send one DM, a few minutes before 10:00 AM and 1:30 PM. It will also constantly update the bot's status with a countdown to the next standup time.
+
+## _Setup/Installation Requirements_
+
+* _Clone this repo_
+* _Run the command `npm i` from the root directory to install the dependancies._
+* _To use this bot you'll need to get a `token` from discord by registering your own bot, and add it to an `.env` file. You'll also need to change the channel and user it messages. I'd recommend using the [discord.js instructions on how to setup and register a bot](https://discordjs.guide/preparations/)._
+
+## _Known Bugs_
+
+* No known bugs
+
+## _Support and contact details_
+
+Create a pull request on GitHub.
+
+## _Technologies Used_
+
+Node.js, npm, Discord.js
+
+### _License_
+
+GPL, keep information free.
+
+Copyright (c) 2021 Brian Hensley
--- index.js
+++ index.js
@@ -3,44 +3,57 @@ const client = new Discord.Client();
 require('dotenv').config();
 
 client.login(process.env.TOKEN);
+let robert = null;
+let general = null;
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    let robert = client.users.fetch(process.env.ROBERT_ID).then(user => { robert = user; });
-    let general = client.channels.fetch(process.env.GENERAL).then(channel => { general = channel; });
-
-    setInterval(() => {
-        let date = new Date();
-        const mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
-        const day = date.getDay();
-        const time = parseInt(`${date.getHours()}${mins}`);
-
-        // alert students
-        if ((time === 957 || time === 1330) && (day !== 0 || day !== 6)) {
-            general.send(`Ding ding @here! Get ready for standup!`);
-            robert.send('Robert, get ready for standup!');
-        }
-
-        // set countdown status
-        let timeString = null;
-        if (time < 1000) {
-            timeString = standupClock(date, "10:00:00");
-        } else if (time >= 1000 && time < 1330) {
-            timeString = standupClock(date, "13:30:00");
-        } else if (time >= 1330) {
-            let tempDate = date;
-            let dayMod = 1;
-            while (dayMod + day === 6 || dayMod + day === 7) { dayMod++; }
-            tempDate.setDate(date.getDate() + dayMod);
-            timeString = standupClock(tempDate, "10:00:00");
-        }
-        if (timeString) client.user.setActivity(`${timeString} until next standup`, { type: 'WATCHING' });
-    }, 60000);
+    client.users.fetch(process.env.ROBERT_ID).then(user => { robert = user; });
+    client.channels.fetch(process.env.GENERAL).then(channel => { general = channel; });
+    alarmClock();
 });
 
-const standupClock = (date, time) => {
-    var timeLeft = new Date(`${date.toDateString()} ${time}`) - new Date();
-    var hoursLeft = Math.floor((timeLeft / (1000 * 60 * 60)));
-    var minutesLeft = Math.floor((timeLeft / 1000) / 60 % 60);
-    return `${hoursLeft} hours ${minutesLeft} minutes.`;
+const alarmClock = () => {
+    const date = new Date();
+    const mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+    const day = date.getDay();
+    const time = parseInt(`${date.getHours()}${mins}`);
+
+    // alert students
+    if ((time === 958 || time === 1328) && (day !== 0 || day !== 6)) {
+        general.send(`:parrot: Squawk @here! Get ready for standup!`);
+        robert.send('Robert, get ready for standup!');
+    }
+
+    // set countdown status
+    let timeString = null;
+    if (time < 1000) {
+        timeString = timeRemaining(date, "10:00:00");
+    } else if (time >= 1000 && time < 1330) {
+        timeString = timeRemaining(date, "13:30:00");
+    } else if (time >= 1330) {
+        let tempDate = date;
+        let dayMod = 1;
+        // loop to add extra days to the countdown if it's Fri or Sat
+        while (dayMod + day === 6 || dayMod + day === 7) { dayMod++; }
+
+        tempDate.setDate(date.getDate() + dayMod);
+        timeString = timeRemaining(tempDate, "10:00:00");
+    }
+    // custom statuses on bots are ignored by discord, 'Watching' activity is the best I can do for a status 
+    if (timeString) client.user.setActivity(`${timeString} until next standup`, { type: 'WATCHING' });
+
+    // this sets a trigger to call alarmClock whenever the cpu clock's seconds are 0, 
+    // updating the bot's status when the clock changes minutes
+    setTimeout(alarmClock, 60000 - (date.getTime() % 60000));
+};
+
+// create a string of hours & mins remaining until next standup
+const timeRemaining = (date, time) => {
+    const timeLeft = new Date(`${date.toDateString()} ${time}`) - new Date();
+    const hoursLeft = Math.floor((timeLeft / (1000 * 60 * 60)));
+    const hs = (hoursLeft !== 1) ? "s" : "";
+    const minutesLeft = Math.ceil((timeLeft / 1000) / 60 % 60);
+    const ms = (minutesLeft !== 1) ? "s" : "";
+    return `${hoursLeft} hour${hs} ${minutesLeft} minute${ms}`;
 };
