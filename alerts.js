5d3ff7c36785d7dbbdc0a75368f9e5176ecbd1e9
Brian Hensley <brnhensley@gmail.com>
2021-04-07 08:47:12 -0700

    + a few standup messages

--- alerts.js
+++ alerts.js
@@ -2,5 +2,12 @@ module.exports = [
     `:parrot: Squawk @here! Get ready for standup!`,
     `:alarm_clock: Ding ding @here! Get ready for standup!`,
     `:hourglass: It's almost time @here! Get ready for standup!`,
-    `:guitar: Let's get ready to rock @here!`
+    `:guitar: Let's get ready to rock @here!`,
+    `:kangaroo: It is almost time hop into standup y'all!`,
+    `:postal_horn: Tooooot, stand up in a few minutes!`,
+    `:crystal_ball: :woman_mage_tone2: I see a standup in your near future!`,
+    `:bell: Ding ding @here! Get ready for standup!`,
+    `:satellite: Incoming message.... "standup in a few minutes!"`,
+    `:bellhop: Ding ding @here! Get ready for standup!`,
+    `:spacedonk: :spacedonk: :spacedonk: Standup in a few! :spacedonk: :spacedonk: :spacedonk: `
 ]
