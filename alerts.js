2b214a9a7b1e60164aa585e0e065e3c04fffc4c4
Brian Hensley <brnhensley@gmail.com>
2021-04-16 13:33:15 -0700

    +@here to alerts

--- alerts.js
+++ alerts.js
@@ -3,11 +3,11 @@ module.exports = [
     `:alarm_clock: Ding ding @here! Get ready for standup!`,
     `:hourglass: It's almost time @here! Get ready for standup!`,
     `:guitar: Let's get ready to rock @here!`,
-    `:kangaroo: It is almost time hop into standup y'all!`,
-    `:postal_horn: Tooooot, stand up in a few minutes!`,
-    `:crystal_ball: :woman_mage_tone2: I see a standup in your near future!`,
+    `:kangaroo: It is almost time hop into standup @here!`,
+    `:postal_horn: Tooooot, stand up in a few minutes @here!`,
+    `:crystal_ball: :woman_mage_tone2: I see a standup in your near future @here!`,
     `:bell: Ding ding @here! Get ready for standup!`,
-    `:satellite: Incoming message.... "standup in a few minutes!"`,
+    `:satellite: Incoming message @here.... "standup in a few minutes!"`,
     `:bellhop: Ding ding @here! Get ready for standup!`,
-    `:warning: :warning: :warning: Standup in a few! :warning: :warning: :warning: `
+    `:warning: :warning: :warning: Standup in a few @here! :warning: :warning: :warning: `
 ]
